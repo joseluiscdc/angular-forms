beb1265955d48a439af71059d2cd10c81746961b
José Luis <joseluis.jlc@gmail.com>
2021-10-17 06:29:12 -0300

    reset

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -8,16 +8,33 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class BasicsComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.minLength(3)]],
-    price: [0, [Validators.required, Validators.min(0) ] ],
-    stock: [0, [Validators.required, Validators.min(0) ] ],
+    name: [ [Validators.required, Validators.minLength(3)]],
+    price: [ [Validators.required, Validators.min(0)]],
+    stock: [ [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.myForm.reset({
+      name: 'Holi',
+      price: 102231,
+      stock: 23
+    })
+  }
 
-  isValid(field: string): boolean | null{
-    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  isValid(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
   }
 }
