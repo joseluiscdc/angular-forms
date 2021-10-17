be114f07227f6bf99e94eccb61b2274366ffe0c4
José Luis <joseluis.jlc@gmail.com>
2021-10-17 08:00:48 -0300

    subscribe changes form

--- src/app/reactive/switchs/switchs.component.ts
+++ src/app/reactive/switchs/switchs.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switchs',
   templateUrl: './switchs.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchsComponent implements OnInit {
-  
+  myForm: FormGroup = this.fb.group({
+    genre: ['M', Validators.required],
+    notifications: [true, Validators.requiredTrue],
+    terms: [false, Validators.requiredTrue],
+  });
 
-  constructor(private fb: FormBuilder) { }
+  person = {
+    genre: 'F',
+    notifications: true,
+  };
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm.reset({ ...this.person, terms: false });
+
+    /*
+    this.myForm.get('terms')?.valueChanges.subscribe((newValue) => {
+      console.log(newValue);
+    });
+    */
+
+    this.myForm.valueChanges.subscribe((form) => {
+      delete form.terms;
+      this.person = form;
+    });
   }
 
+  save() {
+    if (this.myForm.invalid) {
+      return;
+    }
+    const formValue = { ...this.myForm.value };
+    delete formValue.terms;
+    this.myForm = formValue;
+    console.log(this.myForm);
+  }
 }
