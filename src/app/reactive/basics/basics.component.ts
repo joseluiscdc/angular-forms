82e7d186303bfc77ee7ddea89267418e39ee6f0b
José Luis <joseluis.jlc@gmail.com>
2021-10-17 06:21:07 -0300

    Validators

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -8,12 +8,16 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class BasicsComponent implements OnInit {
   myForm: FormGroup = this.fb.group({
-    name: ['Product Test'],
-    price: [15320],
-    stock: [121],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0) ] ],
+    stock: [0, [Validators.required, Validators.min(0) ] ],
   });
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  isValid(field: string): boolean | null{
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
 }
