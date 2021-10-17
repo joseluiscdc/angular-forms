a6695d0b40f3091780db6ca07c424d2504de5ac4
José Luis <joseluis.jlc@gmail.com>
2021-10-17 07:39:32 -0300

    FormBuilder Array

--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 interface Person {
   name: string;
@@ -12,22 +19,54 @@ interface Favorite {
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DinamicsComponent implements OnInit {
-  newGame: string = '';
+  newGame: FormControl = this.fb.control('', [Validators.required, Validators.minLength(3)]);
 
-  person: Person = {
-    name: 'José Luis',
-    favorites: [
-      { id: 1, name: 'Metal Gear' },
-      { id: 2, name: 'Otra wa' },
-    ],
-  };
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    name: ['Joshe', [Validators.required, Validators.minLength(3)]],
+    favorites: this.fb.array(
+      [
+        ['Metal Gear', [Validators.required, Validators.minLength(3)]],
+        ['Otra Wea', [Validators.required, Validators.minLength(3)]],
+      ],
+      [Validators.required, Validators.minLength(2)]
+    ),
+  });
 
-  ngOnInit(): void {
+  get favoritesArr() {
+    return this.myForm.get('favorites') as FormArray;
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  isValid(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.controls.value);
+  }
+
+  addFavorite() {
+    if (this.newGame.invalid) {
+      return;
+    }
+    //this.favoritesArr.push(new FormControl(this.newGame.value, Validators.required));
+    this.favoritesArr.push(this.fb.control(this.newGame.value, [Validators.required, Validators.minLength(3)]));
+    this.newGame.reset();
+  }
+
+  deleteFavorite(index: number){
+    this.favoritesArr.removeAt(index);
+  }
 }
